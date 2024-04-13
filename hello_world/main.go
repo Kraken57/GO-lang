be0647319fb298d881acd920c10e3e74a2244859
Kraken57 <kasaad1609@gmail.com>
2024-04-13 23:20:28 +0530

    Hello World in GO
    -in go pacakage are tightly coupled to directory so we use subdirectory

---
+++ hello_world/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	next "go-learning/myutils"
+)
+
+func main() {
+	fmt.Println("Learn Go Language ")
+
+	next.PrintMessage("Hello World")
+}
---
+++ myutils/myutils.go
@@ -0,0 +1,7 @@
+package next
+
+import "fmt"
+
+func PrintMessage(message string) {
+	fmt.Println(message);
+}
