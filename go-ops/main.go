94dfe341bfb7cd77cac84fd3594740d1c3ee8e54
Kraken57 <kasaad1609@gmail.com>
2024-10-11 21:50:05 +0530

    args slices etc

---
+++ go-ops/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	
+	// fmt.Printf("Hello, World! \nos.Args: %v\nArguments: %v\n", args, args[1:len()])	
+	fmt.Printf("Hello, World! \nos.Args: %v\nArguments: %v\n", args, args[1)	
+}
